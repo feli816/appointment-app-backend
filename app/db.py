cab22a795e7764c0946b8ab5fdaeae2d379336f1
feli816 <alvarezfelipeandres82@gmail.com>
2025-12-07 16:53:31 +0100

    Fix get_session to return SQLModel Session for FastAPI Depends

--- app/db.py
+++ app/db.py
@@ -1,4 +1,3 @@
-from contextlib import contextmanager
 from typing import Iterator
 
 from sqlmodel import Session, SQLModel, create_engine
@@ -13,7 +12,6 @@ def init_db() -> None:
     SQLModel.metadata.create_all(engine)
 
 
-@contextmanager
 def get_session() -> Iterator[Session]:
     with Session(engine) as session:
         yield session
